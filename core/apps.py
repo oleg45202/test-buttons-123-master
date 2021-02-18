b535c0c932be55252f4ea0d1a3cef39fe9fc749a
oleg <gusevoleg96@gmail.ru>
2021-02-18 20:13:49 +0300

    Initial commit

---
+++ core/apps.py
@@ -0,0 +1,14 @@
+import json
+
+from django.apps import AppConfig
+from django.conf import settings
+
+from .companies_redis import RedisClient
+
+
+class CoreConfig(AppConfig):
+    name = 'core'
+
+    def ready(self):
+        pass
+        # RedisClient().set_init_data()
